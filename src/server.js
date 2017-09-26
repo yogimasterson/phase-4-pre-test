df02484e926289e1077a5a9c560a6cd99546bd6d
Michael Masterson <michaelmasterson@Michaels-MacBook-Pro.local>
2017-09-26 11:43:40 -0700

    updated readme and reformated a few files.

--- src/server.js
+++ src/server.js
@@ -1,41 +1,19 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const db = require('./db')
+const routes = require('./server/routes')
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
-require('ejs')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-  db.getAlbums((error, albums) => {
-    if (error) {
-      res.status(500).render('error', {error})
-    } else {
-      res.render('index', {albums})
-    }
-  })
-})
-
-app.get('/albums/:albumID', (req, res) => {
-  const albumID = req.params.albumID
-
-  db.getAlbumsByID(albumID, (error, albums) => {
-    if (error) {
-      res.status(500).render('error', {error})
-    } else {
-      const album = albums[0]
-      res.render('album', {album})
-    }
-  })
-})
+app.use('/', routes)
 
 app.use((req, res) => {
   res.status(404).render('not_found')
---
+++ src/server/routes/index.js
@@ -0,0 +1,27 @@
+const router = require('express').Router()
+const db = require('../../db/index')
+
+router.get('/', (req, res) => {
+  db.getAlbums((error, albums) => {
+    if (error) {
+      res.status(500).render('error', {error})
+    } else {
+      res.render('index', {albums})
+    }
+  })
+})
+
+router.get('/albums/:albumID', (req, res) => {
+  const albumID = req.params.albumID
+
+  db.getAlbumsByID(albumID, (error, albums) => {
+    if (error) {
+      res.status(500).render('error', {error})
+    } else {
+      const album = albums[0]
+      res.render('album', {album})
+    }
+  })
+})
+
+module.exports = router
